0e72a4f42bbf150a082b875cb4d18058cc8cfa98
NathanQuellec <nathan.quellec@gmail.com>
2021-10-26 00:25:40 +0200

    webpack done

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("It works just fine !")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLG1DIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkl0IHdvcmtzIGp1c3QgZmluZSAhXCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
